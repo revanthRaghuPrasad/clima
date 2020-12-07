d0e510c89639b97525007da6a67c1c7065db352c
Revanth R Prasad <revanthrprasad@gmail.com>
2020-12-07 13:07:43 +0530

    particular city weather with the new api call

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -2,8 +2,16 @@ import 'package:clima/services/location.dart';
 import 'package:clima/services/networking.dart';
 
 const apiKey = 'dd60704ac7459d9931225f09b3061dc0';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
 
 class WeatherModel {
+  Future<dynamic> getCityWeather(String cityName) async {
+    var url = '$openWeatherMapURL?q=$cityName&appid=$apiKey&units=metric';
+    NetworkHelper networkHelper = NetworkHelper(url);
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
   Future<dynamic> getLocationWeather() async {
     Location location = Location();
     await location.getCurrentLocation();
